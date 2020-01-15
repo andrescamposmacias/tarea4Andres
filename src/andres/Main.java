e9a975159c6d57555009eeaebfd0a18cda4111f2
SindyF <syohanaferreira@gmail.com>
2020-01-15 13:44:13 +0100

    Añade Main

---
+++ src/andres/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package andres;
+
+/**
+ *
+ * @author sindy
+ */
+public class Main {
+    public static void main(String[] args) {
+        Tarea4Andres uno = new Tarea4Andres();
+        Tarea4Andres dos = new Tarea4Andres(20,50);
+        Tarea4Andres tres = new Tarea4Andres(30,9);
+        
+        System.out.println(uno);
+        System.out.println(dos.toString());
+        System.out.println(tres);
+        
+        
+    }
+}
